6e0df47f50618e7816586834653a597cc561e33c
José Manrique Montero Palma <desoft@da.innovadores>
2026-08-25 11:11:48 -0600

    feat: mostrar todos los productos disponibles en modo catalogo

--- laravel-admin/public/js/products.js
+++ laravel-admin/public/js/products.js
@@ -10,8 +10,9 @@ function getProductsApiUrl() {
 
 function createProductCard(product) {
     const div = document.createElement('div');
-    const isInactive = product.branch_active === false ||
-        (typeof product.branch_active === 'undefined' && (product.active === false || product.active === 0));
+    const catalogMode = !window.selectedBranch;
+    const isInactive = !catalogMode && (product.branch_active === false ||
+        (typeof product.branch_active === 'undefined' && (product.active === false || product.active === 0)));
     div.className = isInactive ? 'menu-item menu-item-inactive' : 'menu-item';
     div.dataset.id = product.id;
     div.dataset.name = product.name;
